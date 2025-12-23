3a3268803f3e65be4cf5584a1e315304189d7208
quantishh <abhi@quantishh.com>
2025-12-23 15:29:18 -0500

    Update script.js

--- geomemo_api/public_site/script.js
+++ geomemo_api/public_site/script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- CHANGE: Point explicitly to your Cloud API Port ---
-    const API_BASE_URL = "http://3.22.27.191:8000";
+    const API_BASE_URL = "https://www.geomemo.news";
     
     // Containers
     const jumpBar = document.getElementById('category-jump-bar');
